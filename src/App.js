495973adfa0d67c9a8e97875c8af28490b3f1998
dhakadpayal <dhakadpayal8932@gmail.com>
2023-07-08 11:04:25 -0700

    F

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import Area
+export default function App(propes){
+  //state
+
+  //fuunction defination
+
+  //return statement 
+  return(
+    <>
+    <h1>Hello {propes.name}</h1>
+    </>
+  )
+}
